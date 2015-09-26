402427f58893798358947fadaa294d58bef97753
manasa884 <manasa.tipparam@gmail.com>
2015-09-26 00:12:26 -0400

    first commit

---
+++ StudyBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyBuddy
+//
+//  Created by Manasa Tipparam on 9/25/15.
+//  Copyright © 2015 Manasa Tipparam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
